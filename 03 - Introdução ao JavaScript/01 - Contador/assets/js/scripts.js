66071f6aeb0bfc209db82425dc7a3156b8341e85
DheniMoura <dhenifer@hotmail.com>
2022-06-17 11:16:31 -0300

    Update scripts.js

--- 03 - Introdução ao JavaScript/01 - Contador/assets/js/scripts.js
+++ 03 - Introdução ao JavaScript/01 - Contador/assets/js/scripts.js
@@ -1,12 +1,27 @@
 var currentNumberWrapper = document.getElementById('currentNumber')
-var currentNumber = 0
+var currentNumber = 0 //valor inicial zero
 
+// função de incremento
 function increment() {
-  currentNumber = currentNumber + 1
-  currentNumberWrapper.innerHTML = currentNumber
+  if (currentNumber < 10) {
+    currentNumber += 1
+    // a cor muda para preto quando clica no botão 'increment'
+    document.getElementById('currentNumber').style.color = 'black'
+
+    return (currentNumberWrapper.innerHTML = currentNumber)
+  }
 }
 
+// função de decremento
 function decrement() {
-  currentNumber = currentNumber - 1
-  currentNumberWrapper.innerHTML = currentNumber
+  if (currentNumber > -10) {
+    currentNumber -= 1
+    //aqui a cor muda quando clica no botão 'decrement'
+    document.getElementById('currentNumber').style.color = 'red'
+
+    return (currentNumberWrapper.innerHTML = currentNumber)
+  }
 }
+
+document.getElementById('increment').addEventListener('click', increment)
+document.getElementById('decrement').addEventListener('click', decrement)

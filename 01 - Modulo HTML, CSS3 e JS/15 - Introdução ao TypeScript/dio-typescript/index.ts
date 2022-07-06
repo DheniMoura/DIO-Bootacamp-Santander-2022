93b0f0a4aa884596c6a07f8daba0da0c96f1e789
DheniMoura <dhenifer@hotmail.com>
2022-07-06 18:30:22 -0300

    Update

--- 01 - Modulo HTML, CSS3 e JS/15 - Introdução ao TypeScript/dio-typescript/index.ts
+++ 01 - Modulo HTML, CSS3 e JS/15 - Introdução ao TypeScript/dio-typescript/index.ts
@@ -1,5 +1,24 @@
-function soma(a: number, b: number){
-    return a + b;
+interface IAnimal {
+    nome: string;
+    tipo: 'terrestre' | 'aquático';
+    executaRugido(alturaEmDecibeis: number): void;
 }
 
-soma('a' + 'b')
+
+interface IFelino extends IAnimal {
+    visaoNoturna: boolean;
+}
+
+const animal: IAnimal = {
+    nome: "Elefante",
+    tipo: "terrestre",
+    executaRugido: (alturaEmDecibeis) => (`${alturaEmDecibeis}dB`)
+} 
+
+animal.executaRugido(10);
+
+const felinio: IFelino = {
+    nome: 'Leão',
+    tipo: "terrestre",
+    visaoNoturna: true,
+}

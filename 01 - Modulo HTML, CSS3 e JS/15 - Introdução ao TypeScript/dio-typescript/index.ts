b31d5156934b34df78b927e51d66a1207cff38c0
DheniMoura <dhenifer@hotmail.com>
2022-07-07 17:24:27 -0300

    Aula TypeScript

--- 01 - Modulo HTML, CSS3 e JS/15 - Introdução ao TypeScript/dio-typescript/index.ts
+++ 01 - Modulo HTML, CSS3 e JS/15 - Introdução ao TypeScript/dio-typescript/index.ts
@@ -1,24 +1,93 @@
-interface IAnimal {
+// tratando a tag input
+/* const input = document.getElementById('input') as HTMLInputElement;
+
+input.addEventListener('input', (event) => {
+    const i = event.currentTarget as HTMLInputElement;
+    console.log(i.value);
+})
+*/
+
+// Generic types
+/*
+function adicionaApendiceALista<generica>(array: any[], valor: generica) {
+    return array.map(item => item + valor);
+}
+
+console.log(adicionaApendiceALista(['A', 'B', 'C'], 'a'));
+*/
+
+// desenvolvendo condicionais a partir de parametros
+/*
+interface IUsuario {
+    id: string;
+    email: string;
+}
+
+interface IAdmin extends IUsuario {
+    cargo: 'gerente' | 'coordenador' | 'supervisor';
+}
+
+function redirecion(usuario: IUsuario | IAdmin) {
+    if('cargo' in usuario) {
+        // redirecionar para a área de adm
+    }
+    // redirecionar para área do usuario
+}
+*/
+
+// desenvolvendo condicionais a partir de parametros utilizando "?" para variáveis opcionais
+/*
+interface IUsuario {
+    id: string;
+    email: string;
+    cargo?: 'funcionario' | 'gerente' | 'coordenador' | 'supervisor'; // valor opcional
+}
+
+function redirecion(usuario: IUsuario) {
+    if (usuario.cargo) {
+        // redirecionar(usuario.cargo)
+    }
+    // redirecionar para a área do usuário externo que não tem cargo
+}
+*/
+
+// criando cariáveis com propriedade readonly
+/*
+interface ICachorro {
     nome: string;
-    tipo: 'terrestre' | 'aquático';
-    executaRugido(alturaEmDecibeis: number): void;
+    idade: number;
+    parqueFavorito?: string;
 }
 
-
-interface IFelino extends IAnimal {
-    visaoNoturna: boolean;
+type CachorroSomenteLeitura = {
+    readonly [K in keyof ICachorro]: ICachorro[K];
 }
 
-const animal: IAnimal = {
-    nome: "Elefante",
-    tipo: "terrestre",
-    executaRugido: (alturaEmDecibeis) => (`${alturaEmDecibeis}dB`)
-} 
+class MeuCachorro implements CachorroSomenteLeitura {
+    readonly idade;
+    readonly nome;
 
-animal.executaRugido(10);
-
-const felinio: IFelino = {
-    nome: 'Leão',
-    tipo: "terrestre",
-    visaoNoturna: true,
+    constructor(nome, idade) {
+        this.nome = nome
+        this.idade = idade;
+    }
 }
+
+const cao = new MeuCachorro ('Apolo', 14);
+
+cao.idade = 9; // tá acusando erro, mas tá mudando igual. Não descobri como corrigir
+
+console.log(cao.idade);
+*/
+
+// importanto bibliotecas externas
+
+import $ from 'jquery';
+
+$.fn.extend({
+    novaFuncao() {
+        console.log('Chamou nova função')
+    }
+});
+
+$('body').novaFuncao();
